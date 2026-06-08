1523a9d36fa66ede9b70135575071f1c0b498c45
Yashwanth <yourgithubemail@gmail.com>
2026-06-08 11:26:28 +0000

    Update frontend homepage

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-Commerce DevOps Project</h1>
+      <h2>CI/CD using Jenkins, Docker & Kubernetes</h2>
+      <p>Deployed by Yashwanth 🚀</p>
     </div>
   );
 }
